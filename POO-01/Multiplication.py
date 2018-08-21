c83ed541134f714fc1867c0cf0f9dac6be268cf0
sistemas <sistemas@mnba-sist-02.mnba.lan>
2018-08-21 13:47:09 -0300

    Create folder

---
+++ POO-01/Multiplication.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class Multiplication:
+	def __init__(self):
+		self.num1 = 0
+		self.num1 = 0
+		self.result = 0
+
+	def calculateMultiplication(self,num1, num2):
+		self.num1 = num1
+		self.num2 = num2
+		result = num1 * num2
+		return result
---
+++ POO-01/Sum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class Sum:
+	def __init__(self):
+		self.num1 = 0
+		self.num1 = 0
+		self.result = 0
+
+	def calculateSum(self,num1, num2):
+		self.num1 = num1
+		self.num2 = num2
+		result = num1 + num2
+		return result
